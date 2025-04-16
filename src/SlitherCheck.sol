23f2382e76e12809ee5c1708c3c182f4f387bc9e
Maikel Ordaz <maikelordaz@gmail.com>
2025-04-16 18:23:14 -0400

    ffirst commit

---
+++ src/SlitherCheck.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.28;
+
+contract SlitherCheck {
+    uint256 public number;
+
+    function setNumber(uint256 newNumber, address toCall) public {
+        (bool success, ) = toCall.call{value: 0}("");
+        number = newNumber;
+    }
+}
